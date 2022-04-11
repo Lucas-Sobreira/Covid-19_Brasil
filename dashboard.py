dc7dba3b248463f3058f97ca1a25f8e9946408b9
Lucas Andrade Sobreira Santos <83103924+Lucas-Sobreira@users.noreply.github.com>
2022-04-11 20:31:34 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -252,4 +252,6 @@ def update_location(click_data, n_clicks):
         return "BRASIL"
 
 if __name__ == "__main__":
-    app.run_server(debug=False, port=5000)
+    # app.run_server(debug=False, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
